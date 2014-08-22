a024163ef6fdea0ba937b278bff056853259801d
Sébastien Vitard <contact@sebastienvitard.name>
2014-08-22 18:23:58 +0200

    Add JS

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+var myNumber = 5; // 5.5, 5.5e+5, 0x391
+var myString = 'HelloWorld'; // "Hello Wayne's world", 'Hello Wayne\'s world'
+var myBoolean = true; // false
+
+var type = typeof myUndefinedVar; // undefined, number, string, boolean
+//var string = prompt('Enter: ');
+myNumber = parseInt('5', 10);
+
+// === (content & type equality), !=== (content or type inequality)
+
+//var boolean = confirm('OK or cancel?');
+
+/*
+0 == false
+'' == false
+undefined == false
+*/
+
+// continue; instruction in a while loop jumps to the next iteration dropping the remaining code
+// loop vars still exists after loop execution...
